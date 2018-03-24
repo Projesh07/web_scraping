5df5fb5c8b8a733a2f875c8f82bb5d93ccdd8392
projesh <projesh.bhoumik07@gmail.com>
2018-03-24 17:28:21 +0600

    scrap error handle v2

---
+++ scrap_error_handle.py
@@ -0,0 +1,49 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+# url='http://shawarmatime.com/mmmmmmmmmm' ## This gives http error
+# url='http://shawarmatime.commm/' ## This gives url error 
+url='http://shawarmatime.com/' #Working url
+
+
+def check_ok(url):
+
+	try:
+		html=urlopen(url)
+	except HTTPError as e:
+		return None
+	except URLError as e:
+		return None	
+	else:
+		bs4object=bs(html.read(),'html.parser')
+	return bs4object
+	
+def check_tag(bs4,img):
+	try:
+		tag=bs4.img
+		print(tag)
+	except AttributeError as e:
+		return None
+	return tag
+
+connect=check_ok(url)
+
+if connect:
+
+	tag=check_tag(connect,'img')
+	if tag:
+		print(tag)
+	else:
+		print("tag not found")
+else:
+	print("error in request")
+
+
+
+		
+
+# bsobject=bs(html)
+
+# print(bsobject.img)
--- test.py
+++ test.py
@@ -1,16 +1,19 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
-from urllib.error import HTTPerror
-from urllib.error import URLerror
+from urllib.error import HTTPError
+from urllib.error import URLError
 
-url='http://shawarmatime.com/'
+# url='http://shawarmatime.com/mmmmmmmmmm' ## This gives http error
+# url='http://shawarmatime.commm/' ## This gives url error 
+url='http://shawarmatime.com/' #Working url
+
 
 try:
 	html=urlopen(url)
-except HTTPerror as e:
-	print(e)
-except URLerror as e:
-	print(e)	
+except HTTPError as e:
+	print("HTTP error {}".format(e))
+except URLError as e:
+	print("URL error {}".format(e))	
 else:
 
 	print("It worked")
