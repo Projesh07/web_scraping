b3fcf5278c5e3e966bea72a28343bf71d3068a03
Projesh Bhoumik <projesh.bhoumik07@gmail.com>
2019-05-22 18:48:29 +0200

    Update finding_by_tag_class.py

--- finding_by_tag_class.py
+++ finding_by_tag_class.py
@@ -5,45 +5,47 @@ from urllib.error import URLError
 
 # url='http://shawarmatime.com/mmmmmmmmmm' ## This gives http error
 # url='http://shawarmatime.commm/' ## This gives url error 
-url='http://shawarmatime.com/' #Working url
+
+class FindTag:
+	url='http://shawarmatime.com/' #Working url
 
 
-def check_ok(url):
+	def check_ok(url):
 
-	try:
-		html=urlopen(url)
-	except HTTPError as e:
-		return None
-	except URLError as e:
-		return None	
+		try:
+			html=urlopen(url)
+		except HTTPError as e:
+			return None
+		except URLError as e:
+			return None	
+		else:
+			bs4object=bs(html.read(),'html.parser')
+		return bs4object
+
+	def check_tag(bs4,search_tag,search_class):
+		try:
+			tag=bs4.findAll(search_tag,{"class":search_class})
+		except AttributeError as e:
+			return None
+		return tag
+
+	connect=check_ok(url)
+
+	if connect:
+
+		tag=check_tag(connect,'div','item')
+		if tag:
+			for item in tag:
+				print(item.get_text())
+		else:
+			print("tag not found")
 	else:
-		bs4object=bs(html.read(),'html.parser')
-	return bs4object
-	
-def check_tag(bs4,search_tag,search_class):
-	try:
-		tag=bs4.findAll(search_tag,{"class":search_class})
-	except AttributeError as e:
-		return None
-	return tag
-
-connect=check_ok(url)
-
-if connect:
-
-	tag=check_tag(connect,'div','item')
-	if tag:
-		for item in tag:
-			print(item.get_text())
-	else:
-		print("tag not found")
-else:
-	print("error in request")
+		print("error in request")
 
 
 
-		
 
-# bsobject=bs(html)
 
-# print(bsobject.img)
+	# bsobject=bs(html)
+
+	# print(bsobject.img)
