756dcde0b8b07614a81390aad280abbffe4a5a84
Projesh Bhoumik <projesh.bhoumik07@gmail.com>
2019-05-22 18:50:47 +0200

    Update scrap_error_handle.py

--- scrap_error_handle.py
+++ scrap_error_handle.py
@@ -5,46 +5,49 @@ from urllib.error import URLError
 
 # url='http://shawarmatime.com/mmmmmmmmmm' ## This gives http error
 # url='http://shawarmatime.commm/' ## This gives url error 
-url='http://shawarmatime.com/' #Working url
 
-
-def check_ok(url):
-
-	try:
-		html=urlopen(url)
-	except HTTPError as e:
-		return None
-	except URLError as e:
-		return None	
-	else:
-		bs4object=bs(html.read(),'html.parser')
-	return bs4object
+class ErrorHandle:
 	
-def check_tag(bs4,ser):
-	try:
-		tag=bs4.find(ser)
-		print(dir(tag))
-		print(tag)
-	except AttributeError as e:
-		return None
-	return tag
+	url='http://shawarmatime.com/' #Working url
 
-connect=check_ok(url)
 
-if connect:
+	def check_ok(url):
 
-	tag=check_tag(connect,'img')
-	if tag:
-		print(tag)
+		try:
+			html=urlopen(url)
+		except HTTPError as e:
+			return None
+		except URLError as e:
+			return None	
+		else:
+			bs4object=bs(html.read(),'html.parser')
+		return bs4object
+
+	def check_tag(bs4,ser):
+		try:
+			tag=bs4.find(ser)
+			print(dir(tag))
+			print(tag)
+		except AttributeError as e:
+			return None
+		return tag
+
+	connect=check_ok(url)
+
+	if connect:
+
+		tag=check_tag(connect,'img')
+		if tag:
+			print(tag)
+		else:
+			print("tag not found")
 	else:
-		print("tag not found")
-else:
-	print("error in request")
+		print("error in request")
 
 
 
-		
 
-# bsobject=bs(html)
 
-# print(bsobject.img)
+	# bsobject=bs(html)
+
+	# print(bsobject.img)
