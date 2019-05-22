ca35d06be1ff1080d63149f6852265ea8facf114
Projesh Bhoumik <projesh.bhoumik07@gmail.com>
2019-05-22 18:47:16 +0200

    Update find_by_regex.py

--- find_by_regex.py
+++ find_by_regex.py
@@ -4,45 +4,45 @@ from urllib.error import HTTPError
 from urllib.error import URLError
 import re
 
-# url='http://shawarmatime.com/mmmmmmmmmm' ## This gives http error
-# url='http://shawarmatime.commm/' ## This gives url error 
-url='http://shawarmatime.com/' #Working url
+	# url='http://shawarmatime.com/mmmmmmmmmm' ## This gives http error
+	# url='http://shawarmatime.commm/' ## This gives url error 
+	url='http://shawarmatime.com/' #Working url
 
+class FindRegx:
+	def check_ok(url):
 
-def check_ok(url):
+		try:
+			html=urlopen(url)
+		except HTTPError as e:
+			return None
+		except URLError as e:
+			return None	
+		else:
+			bs4object=bs(html.read(),'html.parser')
+		return bs4object
 
-	try:
-		html=urlopen(url)
-	except HTTPError as e:
-		return None
-	except URLError as e:
-		return None	
+	def check_tag(bs4,search_tag,search_class):
+		try:
+			tag=bs4.findAll(search_tag,{"class":search_class})
+		except AttributeError as e:
+			return None
+		return tag
+
+	connect=check_ok(url)
+
+	if connect:
+
+		tag=check_tag(connect,'div','images')
+		regx=re.compile("\.jpg")
+
+		if tag:
+			for item in tag:
+				for i in item.findAll('img',{'src':regx}):
+					print(i['src'])
+		else:
+			print("tag not found")
 	else:
-		bs4object=bs(html.read(),'html.parser')
-	return bs4object
-	
-def check_tag(bs4,search_tag,search_class):
-	try:
-		tag=bs4.findAll(search_tag,{"class":search_class})
-	except AttributeError as e:
-		return None
-	return tag
-
-connect=check_ok(url)
-
-if connect:
-
-	tag=check_tag(connect,'div','images')
-	regx=re.compile("\.jpg")
-
-	if tag:
-		for item in tag:
-			for i in item.findAll('img',{'src':regx}):
-				print(i['src'])
-	else:
-		print("tag not found")
-else:
-	print("error in request")
+		print("error in request")
 
 
 
